bf6596aa8141d90cf96f9a04ed4258bc8c6c537a
Mike Fandorin <crespo8800@gmail.com>
2015-03-14 22:46:47 +0200

    Finish

--- app/scripts/game/game.js
+++ app/scripts/game/game.js
@@ -1,108 +1,126 @@
 'use strict';
 
-angular
-	.module('Game', ['Grid', 'ngCookies'])
-	.service('GameManager', function(GridService, $cookieStore){
-	  
-	  var self = this;
+angular.module('Game', ['Grid', 'ngCookies'])
+.service('GameManager', function($q, $timeout, GridService, $cookieStore) {
 
-	  self.getHighScore = function() {
-		  return parseInt($cookieStore.get('highScore')) || 0;
-		};
+  this.getHighScore = function() {
+    return parseInt($cookieStore.get('highScore')) || 0;
+  };
 
-	  // Create a new game
-	  self.newGame = function() {
-	  	GridService.buildEmptyGameBoard();
-	    GridService.buildStartingPosition();
-	    this.reinit();
-	  };
+  this.grid = GridService.grid;
+  this.tiles = GridService.tiles;
+  this.gameSize = GridService.getSize();
 
-	  // Handle the move action
-	  self.move = function(key) {
-		  var self = this;
-		  var hasWon = false;
-		  var hasMoved = false;
+  this.winningValue = 2048;
 
-		  // define move here
-		  if (self.win) { return false; }
-		  var positions = GridService.traversalDirections(key);
+  this.reinit = function() {
+    this.gameOver = false;
+    this.win = false;
+    this.currentScore = 0;
+    this.highScore = this.getHighScore();
+  };
+  this.reinit();
 
-		  positions.x.forEach(function(x) {
-		    positions.y.forEach(function(y) {
-		      // For every position
-		      // save the tile's original position
-				var originalPosition = {x:x,y:y};
-				var tile = GridService.getCellAt(originalPosition);
+  this.newGame = function() {
+    GridService.buildEmptyGameBoard();
+    GridService.buildStartingPosition();
+    this.reinit();
+  };
 
-				if (tile) {
-				  // if we have a tile here
-				  var 	cell = GridService.calculateNextPosition(tile, key),
-				  		next = cell.next;
+  /*
+   * The game loop
+   *
+   * Inside here, we'll run every 'interesting'
+   * event (interesting events are listed in the Keyboard service)
+   * For every event, we'll:
+   *  1. look up the appropriate vector
+   *  2. find the furthest possible locations for each tile and 
+   *     the next tile over
+   *  3. find any spots that can be 'merged'
+   *    a. if we find a spot that can be merged:
+   *      i. remove both tiles
+   *      ii. add a new tile with the double value
+   *    b. if we don't find a merge:
+   *      i. move the original tile
+   */
+  this.move = function(key) {
+    var self = this;
+    var f = function() {
+      if(self.win) { return false; }
+      var positions = GridService.traversalDirections(key);
+      var hasMoved = false;
+      var hasWon = false;
 
-					  if (next &&
-					      next.value === tile.value &&
-					      !next.merged) {
-					    // Handle merged
-						var newValue = tile.value * 2;
-						// Create a new tile
-						var mergedTile = GridService.newTile(tile, newValue);
-						mergedTile.merged = [tile, cell.next];
-						// Insert the new tile
-						GridService.insertTile(mergedTile);
-						// Remove the old tile
-						GridService.removeTile(tile);
-						// Move the location of the mergedTile into the next position
-						GridService.moveTile(mergedTile, next);
-						// Update the score of the game
-						self.updateScore(self.currentScore + newValue);
-						// Check for the winning value
-						if (mergedTile.value >= self.winningValue) {
-						  hasWon = true;
-						}
-						hasMoved = true; // we moved with a merge
-					  } else {
-					    GridService.moveTile(tile, cell.newPosition);
-					  }
+      // Update Grid
+      GridService.prepareTiles();
 
-					  if (!GridService.samePositions(originalPosition, cell.newPosition)) {
-						  hasMoved = true;
-						}
+      positions.x.forEach(function(x) {
+        positions.y.forEach(function(y) {
+          var originalPosition = {x:x,y:y};
+          var tile = GridService.getCellAt(originalPosition);
 
-						if (hasMoved) {
-						  GridService.randomlyInsertNewTile();
+          if (tile) {
+            var cell = GridService.calculateNextPosition(tile, key),
+                next = cell.next;
 
-						  if (self.win || !self.movesAvailable()) {
-						    self.gameOver = true;
-						  }
-						}
+            if (next &&
+                next.value === tile.value &&
+                !next.merged) {
 
-				}
-		    });
-		  });
-		};
-	  
-	  // Update the score
-	  self.updateScore = function(newScore) {
-	  	this.currentScore = newScore;
-		  if (this.currentScore > this.getHighScore()) {
-		    this.highScore = newScore;
-		    // Set on the cookie
-		    $cookieStore.put('highScore', newScore);
-		  }
-	  };
-	  
-	  // Are there moves left?
-	  self.movesAvailable = function() {
-	  	return GridService.anyCellsAvailable() || 
-            GridService.tileMatchesAvailable();
-	  };
+              // MERGE
+              var newValue = tile.value * 2;
 
-		// Reset game state
-	  self.reinit = function() {
-	    this.gameOver = false;
-	    this.win = false;
-	    this.currentScore = 0;
-	    this.highScore = 0; // we'll come back to this
-	  };
+              var merged = GridService.newTile(tile, newValue);
+              merged.merged = [tile, cell.next];
 
-	});
+              GridService.insertTile(merged);
+              GridService.removeTile(tile);
+
+              GridService.moveTile(merged, next);
+
+              self.updateScore(self.currentScore + cell.next.value);
+
+              if(merged.value >= self.winningValue) {
+                hasWon = true;
+              }
+              hasMoved = true; // we moved with a merge
+            } else {
+              GridService.moveTile(tile, cell.newPosition);
+            }
+
+            if (!GridService.samePositions(originalPosition,cell.newPosition)) {
+              hasMoved = true;
+            }
+          }
+        });
+      });
+
+      if (hasWon && !self.win) {
+        self.win = true;
+      }
+
+      if (hasMoved) {
+        GridService.randomlyInsertNewTile();
+
+        if (!self.movesAvailable()) {
+          self.gameOver = true;
+        }
+      }
+
+    };
+    return $q.when(f());
+  };
+
+  this.movesAvailable = function() {
+    return GridService.anyCellsAvailable() || GridService.tileMatchesAvailable();
+  };
+
+  this.updateScore = function(newScore) {
+    this.currentScore = newScore;
+    if(this.currentScore > this.getHighScore()) {
+      this.highScore = newScore;
+      $cookieStore.put('highScore', newScore);
+    }
+  };
+
+});
--- app/scripts/grid/tile_directive.js
+++ app/scripts/grid/tile_directive.js
@@ -1,13 +1,12 @@
 'use strict';
 
-angular
-	.module('Grid')
-	.directive('tile', function(){
-		return {
-			restrict: 'A',
-		    scope: {
-		      ngModel: '='
-		    },
-		    templateUrl: 'scripts/grid/tile.html'
-		};
-	});
+angular.module('Grid')
+.directive('tile', function() {
+  return {
+    restrict: 'A',
+    scope: {
+      ngModel: '='
+    },
+    templateUrl: 'scripts/grid/tile.html'
+  };
+});
